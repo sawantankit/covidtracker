035fe7f68d81067326c9280378896b764d5d0eaa
Ankit Sharad <Ankit@gmail.com>
2020-08-09 18:31:06 -0400

    Fixed Line Chart Date of Confirmed and Deaths

--- src/api/index.js
+++ src/api/index.js
@@ -21,7 +21,7 @@ export const fetchDailyData = async () => {
         const modifiedData = data.map((dailyData) => ({
              confirmed: dailyData.confirmed.total, 
              deaths: dailyData.deaths.total, 
-             date: dailyData.reportData,
+             date: dailyData.reportDate,
             }));
         
             return modifiedData;
