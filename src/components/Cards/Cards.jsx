5557559000120392cfc2b7f453b76c6bee3cd946
Ankit Sharad <Ankit@gmail.com>
2020-08-08 23:42:38 -0400

    Adding  Count functionality as well as displaying the number of Confirmed, Recovered and Deaths COVID-19 cases
    Changes made in Cards Component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,70 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup';
 
-const Cards = () => {
-    return(
-        <h1>Cards</h1>
+
+import styles from './Cards.module.css';
+
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+
+    if (!confirmed) {
+        return ("Loading...")
+
+    }
+    return (
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom> INFECTED </Typography>
+                        <Typography variant="h5">
+                            <CountUp
+                                start={0}
+                                end={confirmed.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
+                        <Typography color="textSecondary"> {new Date(lastUpdate).toDateString()}  </Typography>
+                        <Typography variant="body2">NO OF ACTIVE COVID-19 CASES</Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom> RECOVERED </Typography>
+                        <Typography variant="h5">
+                            <CountUp
+                                start={0}
+                                end={recovered.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
+                        <Typography color="textSecondary"> {new Date(lastUpdate).toDateString()} </Typography>
+                        <Typography variant="body2">NO OF RECOVERED COVID-19 CASES</Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom> DEATHS </Typography>
+                        <Typography variant="h5">
+                            <CountUp
+                                start={0}
+                                end={deaths.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
+                        <Typography color="textSecondary"> {new Date(lastUpdate).toDateString()}  </Typography>
+                        <Typography variant="body2">NO OF DEATHS CAUSED BY COVID-19 </Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+
+
+        </div>
     )
 
 }
